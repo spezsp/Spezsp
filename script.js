c6b01263be5cbf1c88ec8e605ff45be70a32026f
Pavlik <spezsp@yahoo.com>
2016-04-27 21:51:15 +0300

    slight styling

---
+++ script.js
@@ -0,0 +1,94 @@
+var Fcalc = document.calc;
+var Currents = 0;
+var FlagNewNum = false;
+var PendingOp = "";
+
+
+// Btn Press Function
+function NumPressed (Num) 
+{
+		if (FlagNewNum) 
+		{
+			Fcalc.ReadOut.value = Num;
+			FlagNewNum = false;
+		}	
+		else 
+		{
+			if (Fcalc.ReadOut.value == "0")
+				Fcalc.ReadOut.value = Num;
+			else
+				Fcalc.ReadOut.value += Num;
+		}
+}
+	
+// Action Btn Function
+function Operation (Op) 
+{
+		var Readout = Fcalc.ReadOut.value;
+		if (FlagNewNum && PendingOp != "=")
+		{
+			Fcalc.ReadOut.value = Currents;
+		}
+		else
+		{
+			FlagNewNum = true;
+			if ( '+' == PendingOp )
+				Currents += parseFloat(Readout);
+			else if ( '-' == PendingOp )
+				Currents -= parseFloat(Readout);
+			else if ( '/' == PendingOp )
+				Currents /= parseFloat(Readout);
+			else if ( '*' == PendingOp )
+				Currents *= parseFloat(Readout);
+			else
+				Currents = parseFloat(Readout);
+			Fcalc.ReadOut.value = Currents;
+			PendingOp = Op;
+		}
+}
+	
+// Decimal point sign Add
+function Decimal () 
+{
+		var curReadOut = Fcalc.ReadOut.value;
+		if (FlagNewNum) 
+		{
+			curReadOut = "0.";
+			FlagNewNum = false;
+		}
+		else
+		{
+			if (curReadOut.indexOf(".") == -1)
+				curReadOut += ".";
+		}
+		Fcalc.ReadOut.value = curReadOut;
+}
+	
+function ClearEntry () 
+{
+		Fcalc.ReadOut.value = "0";
+		FlagNewNum = true;
+}
+	
+function Clear () 
+{
+		Currents = 0;
+		PendingOp = "";
+		ClearEntry();
+
+}
+
+// Sign change 
+function Neg () 
+{
+		Fcalc.ReadOut.value = 
+		parseFloat(Fcalc.ReadOut.value) * -1;
+}
+	
+// Percent
+function Percent () 
+{
+		Fcalc.ReadOut.value = 
+			(parseFloat(Fcalc.ReadOut.value) / 100) * 
+			parseFloat(Currents);
+}
